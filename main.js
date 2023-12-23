196295051198f2eda45c299bb7372d71f822ad99
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2023-12-23 23:17:54 +0500

    yana qoshdim

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const player = document.querySelector("#play");
 const tugma1 = document.querySelector("#btn1");
 const tugma2 = document.querySelector("#btn2");
+const tugmaTepa = document.querySelector("#btn3")
+const tugmaPast = document.querySelector("#btn4")
 
 tugma1.addEventListener("click", ()=> {
     player.classList.add("chap");
@@ -11,6 +13,16 @@ tugma2.addEventListener("click", ()=> {
     player.classList.add("ong");
     player.classList.remove("chap");
 });
+
+tugmaTepa.addEventListener("click", ()=> {
+    player.classList.add("tepa");
+    player.classList.remove("past");
+});
+
+tugmaPast.addEventListener("click", ()=> {
+    player.classList.add("past");
+    player.classList.remove("tepa")
+})
 
 // document.addEventListener("keydown", (e)=> {
 //     if (e.keycode === 37) {
