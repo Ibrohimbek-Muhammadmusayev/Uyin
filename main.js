dfb6863175cf7c08ebb4adef966b27a4e5c6fb62
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2024-01-07 20:41:24 +0500

    endi tugmalarda ishlaydi

--- main.js
+++ main.js
@@ -16,24 +16,62 @@ tugma1.addEventListener("click", ()=> {
     player.classList.remove("orta");
 });
 
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "a") {
+        player.classList.remove("chap");
+        player.classList.add("ong");
+        player.classList.remove("orta");
+    }
+});
+
 tugma2.addEventListener("click", ()=> {
     player.classList.remove("ong");
     player.classList.add("chap");
     player.classList.remove("orta");
 });
 
+
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "d") {
+        player.classList.remove("ong");
+        player.classList.add("chap");
+        player.classList.remove("orta");
+    }
+});
+
+
 tugmaTepa.addEventListener("click", ()=> {
     player.classList.add("tepa");
     player.classList.remove("past");
+});
+
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "w") {
+        player.classList.add("tepa");
+        player.classList.remove("past");
+    }
 });
 
 tugmaOrta.addEventListener("click", ()=> {
     player.classList.add("orta");
 })
 
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "f") {
+        player.classList.add("orta");
+    }
+});
+
 tugmaPast.addEventListener("click", ()=> {
     player.classList.add("past");
     player.classList.remove("tepa")
+});
+
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "s") {
+        player.classList.add("past");
+        player.classList.remove("tepa")
+    }
 });
 
 BirinchiSamlyot.addEventListener("click", ()=> {
@@ -44,12 +82,32 @@ BirinchiSamlyot.addEventListener("click", ()=> {
     TortinchiSamlyot.classList.remove("bosildi")
 });
 
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "1") {
+        player.setAttribute("src", "/rasim/1705987.png");
+        BirinchiSamlyot.classList.add("bosildi")
+        IkkinchiSamlyot.classList.remove("bosildi")
+        UchinchiSamlyot.classList.remove("bosildi")
+        TortinchiSamlyot.classList.remove("bosildi")
+    }
+});
+
 IkkinchiSamlyot.addEventListener("click", ()=> {
     player.setAttribute("src", "/rasim/8025480.png");
     BirinchiSamlyot.classList.remove("bosildi")
     IkkinchiSamlyot.classList.add("bosildi")
     UchinchiSamlyot.classList.remove("bosildi")
     TortinchiSamlyot.classList.remove("bosildi")
+});
+
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "2") {
+        player.setAttribute("src", "/rasim/8025480.png");
+        BirinchiSamlyot.classList.remove("bosildi")
+        IkkinchiSamlyot.classList.add("bosildi")
+        UchinchiSamlyot.classList.remove("bosildi")
+        TortinchiSamlyot.classList.remove("bosildi")
+    }
 });
 
 UchinchiSamlyot.addEventListener("click", ()=> {
@@ -60,6 +118,16 @@ UchinchiSamlyot.addEventListener("click", ()=> {
     TortinchiSamlyot.classList.remove("bosildi")
 });
 
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "3") {
+        player.setAttribute("src", "/rasim/gamelogo3.svg");
+        BirinchiSamlyot.classList.remove("bosildi")
+        IkkinchiSamlyot.classList.remove("bosildi")
+        UchinchiSamlyot.classList.add("bosildi")
+        TortinchiSamlyot.classList.remove("bosildi")
+    }
+});
+
 TortinchiSamlyot.addEventListener("click", ()=> {
     player.setAttribute("src", "/rasim/icon.webp");
     BirinchiSamlyot.classList.remove("bosildi")
@@ -68,8 +136,12 @@ TortinchiSamlyot.addEventListener("click", ()=> {
     TortinchiSamlyot.classList.add("bosildi")
 });
 
-// document.addEventListener("keydown", (e)=> {
-//     if (e.keycode === 37) {
-//         player.classList.add("ong");
-//     }
-// })
+document.addEventListener("keyup", (e)=> {
+    if (e.key == "4") {
+        player.setAttribute("src", "/rasim/icon.webp");
+        BirinchiSamlyot.classList.remove("bosildi")
+        IkkinchiSamlyot.classList.remove("bosildi")
+        UchinchiSamlyot.classList.remove("bosildi")
+        TortinchiSamlyot.classList.add("bosildi")
+    }
+});
